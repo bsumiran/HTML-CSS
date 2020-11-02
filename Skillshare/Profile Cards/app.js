8fa0c78c7671d1baded4205295b7a64c63779c9e
bsumiran <63335442+bsumiran@users.noreply.github.com>
2020-11-02 22:47:45 +0545

    Add files via upload

---
+++ Skillshare/Profile Cards/app.js
@@ -0,0 +1,24 @@
+const heading='Please Meet Our Team';
+
+let i=0;
+
+const typing= ()=>{
+    if (i<heading.length){
+        document.querySelector('.heading').innerHTML+=heading.charAt(i);
+        i++;
+
+        setTimeout(typing,150);
+    }
+}
+typing();
+
+
+
+
+
+
+
+
+
+
+
